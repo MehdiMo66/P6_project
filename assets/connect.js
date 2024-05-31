98b5eda135f4b2bd24ab4363998928fa2c6f9008
medi <mehdi.mohammedi@hotmail.fr>
2024-06-01 00:12:54 +0200

    final

--- assets/connect.js
+++ assets/connect.js
@@ -1,51 +1,45 @@
-   
-   if(sessionStorage.getItem('token')){
+if (sessionStorage.getItem('token')) {
     sessionStorage.removeItem('token');
-   // window.location.href = "../index.html";
-   }
-   
-   
-   
-   
-   
-   const Connect = document.querySelector(".connexion")
-    
-    Connect.addEventListener("submit", (event) => {
+}
 
-        event.preventDefault();
-        const User = {
-            email: document.getElementById('name').value,
-            password: document.getElementById('password').value
+const Connect = document.querySelector(".connexion")
+
+Connect.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+    const User = {
+        email: document.getElementById('name').value,
+        password: document.getElementById('password').value
+    }
+
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(User)
+    }).then((reponse) => {
+        if (!reponse.ok) {
+            console.log('not allowed');
+            if (reponse.status === 401) {
+                throw new Error('Erreur dans l’identifiant ou le mot de passe');
+            }
+            throw new Error('erreur')
         }
+        return reponse.json()
 
+    }).then((data) => {
+        if (data.token) {
+            sessionStorage.setItem('token', data.token);
+            window.location.href = "/index.html";
 
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(User)
-        }).then((reponse) => {
-            if (!reponse.ok) {
-                console.log('not allowed');
-                if (reponse.status === 401) {
-                    throw new Error('Erreur dans l’identifiant ou le mot de passe');
-                }
-                throw new Error('erreur')
-            }
-            return reponse.json()
-
-        }).then((data) => {
-            if (data.token) {
-                sessionStorage.setItem('token', data.token);
-                window.location.href = "/index.html";
-                
-            }
-            })
+        }
+    })
 
         .catch((error) => {
-                const id = document.querySelector('.error')
-                    .innerHTML = error.message
-              
-            })
-    })
+            const id = document.querySelector('.error')
+                .innerHTML = error.message
+
+        })
+})
 
 
--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,35 @@ let gallery = document.querySelector('.gallery');
 let filters = document.querySelector('.filters');
 let Button = document.querySelector('.button');
 
+let Categorie = document.getElementById('Categorie');
+const header = document.querySelector('header')
+const portfolio = document.getElementById('portfolio');
+const h2 = document.querySelector('.projet');
+
+const token = sessionStorage.getItem('token');
+
+const modal = document.querySelector('.modal');
+const modalAdd = document.querySelector('.modal_add');
+const modalSend = document.querySelector('.photo-upload');
+
+const arrowLeft = document.querySelector('.gauche');
+const ajoutPhoto = document.querySelector('.button_selectedx');
+
+const inputPhoto = document.getElementById('btn_add');
+const images = document.getElementById('images');
+const target = document.getElementById('Categorie');
+
+const fichier = document.getElementById('btn_add');
+const titre = document.getElementById('title');
+const valider = document.getElementById('valider');
+
+const login = document.querySelector('a[href="./login.html"]')
+const sombre = document.querySelector('.sombre')
+
+let allFilters = document.createElement('button');
+allFilters.className = 'button_selected';
+allFilters.innerText = 'Tous';
+filters.appendChild(allFilters);
 
 // recuperation des donnees de l'api
 const getWork = () => {
@@ -11,19 +40,17 @@ const getWork = () => {
         .then((data) => {
             work = data;
             getPictures('.gallery', work, false);
-            getCategory();
         })
 }
-
 
 const getPictures = (elem, data, edit) => {
     const image = document.querySelector(elem)
     image.innerHTML = '';
     data.forEach((w) => {
-
         const figure = document.createElement('figure');
         const img = document.createElement('img')
         img.setAttribute('src', w.imageUrl)
+
         if (edit) {
             const i = document.createElement('i')
             i.className = "fa-solid fa-trash-can"
@@ -31,7 +58,9 @@ const getPictures = (elem, data, edit) => {
             i.addEventListener('click', (event) => {
                 const deletePic = event.target.parentNode
                 deletePic.remove()
-                console.log(w.id)
+                let BtnSelected = document.querySelector('.button_selected');
+                BtnSelected.classList.remove(`button_selected`)
+                allFilters.classList.add(`button_selected`);
 
                 fetch(`http://localhost:5678/api/works/${w.id}`, {
                     method: "DELETE",
@@ -47,14 +76,11 @@ const getPictures = (elem, data, edit) => {
                     }
                     getWork();//verifier le statut 200 204 avant de lancer la fonction
                     return reponse.json()
-
-
                 }).then((data) => {
-                    console.log(data)
+                    // console.log(data)
                 })
                     .catch((error) => {
                         console.log(error.message)
-
                     })
             })
         }
@@ -68,26 +94,32 @@ const getPictures = (elem, data, edit) => {
     })
 }
 
-
 const getCategory = () => {
-    filters.innerHTML = ''
-    let cat = [];//Creation d'un tableau pour stocker les categories et eviter les doublons
-    let allFilters = document.createElement('button');
-    allFilters.className = 'button_selected';
-    allFilters.innerText = 'Tous';
-    filters.appendChild(allFilters);
+    fetch('http://localhost:5678/api/categories', {
+        method: 'GET',
+        headers: { "Content-Type": "application/json" },
+    }).then((rep) => {
 
-    work.forEach((w) => {
-        if (!cat[w.category.name]) {//Si dans la boucle le nom de cette categorie n'existe pas dans la variable 'cat',alors on l'ajoute dans celle-ci et on cree son bouton 
-            cat[w.category.name] = Number(w.category.id);//j'utiliste number pour m'assurer qu'il passe en 'int' dans le tableau
+        return rep.json();
+    }).then((data) => {
+        console.log('tableau complet')
+        console.log(data)
+
+        data.forEach((w) => {
             let Button = document.createElement('button');
-            Button.innerText = w.category.name;
-            Button.id = w.category.id;
+            Button.innerText = w.name;
+            Button.id = w.id;
             filters.appendChild(Button);
 
-        }
-    })
+            const option = document.createElement('option')
+            option.value = w.id
+            option.innerText = w.name
+            target.appendChild(option)
+        })
+    }
+    )
 }
+
 
 filters.addEventListener("click", (event) => {
     //Je m'assure d'etre dans le bouton de la class '.filters'
@@ -95,7 +127,7 @@ filters.addEventListener("click", (event) => {
         let BtnSelected = document.querySelector('.button_selected');//Je pointe sur l'element DOM qui a la class 'button_selected'
         BtnSelected.classList.remove(`button_selected`);//Je retire la class de l'element DOM selectionne precedemment
         event.target.classList.add(`button_selected`);//J'ajoute la class a l'objet DOM sur lequel j'ai cliqué
-        console.log(BtnSelected)
+
         if (event.target.id) {//Si un id est existant, alors je filtre les categories 
             const ClickFilter = work.filter(function (item) {
                 return item.category.id == event.target.id;//Je compare les id des item a l'id de la categorie(event.target.id)pour uniquement retourner ceux qui correspondent
@@ -103,21 +135,14 @@ filters.addEventListener("click", (event) => {
             getPictures('.gallery', ClickFilter, false);//J'apelle la fonction pour afficher les resultat filtrees dans ClickFilter
 
         } else {//Si aucun id est existant, j'affiche tout
-            getPictures('.gallery', work, false);
+
+            return getPictures('.gallery', work, false);
         }
     }
 })
 
-
-const header = document.querySelector('header')
-const portfolio = document.getElementById('portfolio')
-const h2 = document.querySelector('h2')
-const token = sessionStorage.getItem('token')
-const modal = document.querySelector('.modal');
-const modalAdd = document.querySelector('.modal_add')
-//sessionStorage.removeItem('token')
 if (token) {
-    const login = document.querySelector('a[href="./login.html"]')
+
     login.innerText = 'logout'
 
     const admin = document.createElement('div')
@@ -132,35 +157,34 @@ if (token) {
 
     modifierPhoto.addEventListener('click', () => {
         modal.style.display = 'block'
+        sombre.style.display = 'block'
         getPictures('.image', work, true)
     })
-    /*
-        const clickFerme = document.querySelectorAll('.cross')
-    
-        clickFerme.addEventListener('click', ()=> {
-            modal.style.display= 'none';
-            modalAdd.style.display='none';
-            console.log(modalAdd)
-        })*/
+
     document.querySelectorAll('.cross').forEach((cross) => {
         cross.addEventListener('click', () => {
             modal.style.display = 'none';
             modalAdd.style.display = 'none';
+            sombre.style.display = 'none'
             console.log(modalAdd)
         })
     })
 
-    const arrowLeft = document.querySelector('.gauche')
     arrowLeft.addEventListener('click', () => {
         modalAdd.style.display = 'none';
         modal.style.display = 'block';
     })
 
 
-    const ajoutPhoto = document.querySelector('.button_selectedx')
     ajoutPhoto.addEventListener('click', () => {
+        images.style.display = 'none'
+        modalSend.reset()
         modal.style.display = 'none'
         modalAdd.style.display = 'block'
+        valider.style.backgroundColor = 'rgb(203, 214, 220)'
+        let BtnSelected = document.querySelector('.button_selected');
+        BtnSelected.classList.remove(`button_selected`)
+        allFilters.classList.add(`button_selected`);
     })
 
     const bandeNoire = document.createElement('div')
@@ -168,28 +192,62 @@ if (token) {
     bandeNoire.innerHTML = `<span><i class="fa-regular fa-pen-to-square"></i> Mode édition</span>`
     header.prepend(bandeNoire)
 
-    const modalSend = document.querySelector('.photo-upload')
     modalSend.addEventListener("submit", (event) => {
         event.preventDefault();
-        const elemModal = {
-            image: document.getElementById('btn_add').value,
-            title: document.getElementById('title').value,
-            category: parseInt(document.getElementById('Categorie').value)
-        }
+
+        image = document.getElementById('btn_add').files[0],
+            title = document.getElementById('title').value,
+            categoryId = parseInt(document.getElementById('Categorie').value)
+
+        const Data = new FormData();
+
+        Data.append('image', image)
+        Data.append('title', title)
+        Data.append('category', categoryId)
+
         fetch('http://localhost:5678/api/works', {
             method: 'POST',
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(elemModal)
-        })
-        console.log(elemModal)
+            headers: { "Authorization": `Bearer ${token}` },
+            body: Data
+        }).then((rep) => {
+            if ((rep.ok) && (rep.status === 201)) {
+                getWork()
+                modalAdd.style.display = 'none'
+                sombre.style.display = 'none'
+            }
+            return rep.json();
+        }).then((data) => {
 
+            console.log(data)
+        })
     })
+
+    inputPhoto.addEventListener("change", (event) => {
+
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            images.src = e.target.result;
+
+        };
+        reader.readAsDataURL(file);
+        images.style.display = 'block';
+    });
 }
 
+const play = () => {
 
+    if ((fichier.files.length > 0) && (titre.value.length > 0)) {
+        valider.style.backgroundColor = '#1D6154'
+        valider.disabled = false
+    } else {
+        valider.style.backgroundColor = '#CBD6DC'
+        valider.disabled = true
+    }
+}
+titre.addEventListener('input', play)
+fichier.addEventListener('change', play)
 
-
+getCategory();
 getWork();
